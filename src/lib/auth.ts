accdd419c2d53a4986fcdf038ece640a6ba9a63f
Raihan <raihanhn20@gmail.com>
2023-11-23 03:28:31 +0600

    google auth added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { db } from "./db";
 import { compare } from "bcrypt";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
@@ -13,6 +14,11 @@ export const authOptions: NextAuthOptions = {
     signIn: "/sign-in",
   },
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+
     CredentialsProvider({
       name: "Credentials",
       credentials: {
@@ -30,13 +36,15 @@ export const authOptions: NextAuthOptions = {
         if (!existingUser) {
           return null;
         }
-        const passwordMatch = await compare(
-          credentials.password,
-          existingUser.password
-        );
 
-        if (!passwordMatch) {
-          return null;
+        if (existingUser.password) {
+          const passwordMatch = await compare(
+            credentials.password,
+            existingUser.password
+          );
+          if (!passwordMatch) {
+            return null;
+          }
         }
 
         return {
