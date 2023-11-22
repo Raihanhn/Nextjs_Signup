024220b0cae7919c11783b1bb57a3be2de69615b
Raihan <raihanhn20@gmail.com>
2023-11-23 03:35:12 +0600

    env added to gitignore

--- src/components/GoogleSigninButton.tsx
+++ src/components/GoogleSigninButton.tsx
@@ -1,12 +1,14 @@
 import React, { FC, ReactNode } from "react";
 import { Button } from "./ui/button";
+import { signIn } from "next-auth/react";
 
 interface GoogleSigninButtonProps {
   children: ReactNode;
 }
 
 const GoogleSigninButton: FC<GoogleSigninButtonProps> = ({ children }) => {
-  const loginWithGoogle = () => console.log("login with google");
+  const loginWithGoogle = () =>
+    signIn("google", { callbackUrl: "http://localhost:3000/admin" });
   return (
     <Button onClick={loginWithGoogle} className="w-full">
       {" "}
